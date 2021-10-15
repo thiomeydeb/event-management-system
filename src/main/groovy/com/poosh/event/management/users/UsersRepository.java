1e94f642de1b97f0ddc94c71f8e6b526d773de3c
thiomeydeb <63797270+thiomeydeb@users.noreply.github.com>
2021-10-15 12:50:59 +0300

    Added method to search users by company name

--- src/main/groovy/com/poosh/event/management/users/UsersRepository.java
+++ src/main/groovy/com/poosh/event/management/users/UsersRepository.java
@@ -3,8 +3,11 @@ package com.poosh.event.management.users;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
-    @Repository
+
+@Repository
     public interface UsersRepository extends CrudRepository<Users, Long>{
         void updateUsersStatus(Long id, boolean status);
+        Optional<Users> findUsersByCompany_nameAndActiveOrderById(String email);
 }

831e4b6c98e1915e67322f5b12e801e871658410
MM <you@example.com>
2021-11-29 11:55:04 +0300

    1. Updated roles crud functionality 2. Updated JavaMail properties

--- src/main/groovy/com/poosh/event/management/adminrole/AdminRoleController.java
+++ src/main/groovy/com/poosh/event/management/adminrole/AdminRoleController.java
@@ -1,7 +1,12 @@
 package com.poosh.event.management.adminrole;
 
+import com.poosh.event.management.apiresponse.BaseApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("api/v1/admin-role")
@@ -14,19 +19,49 @@ public class AdminRoleController {
         this.adminRoleService = adminRoleService;
     }
 
-    @GetMapping
-    public void getAllAdminRole(){
-        adminRoleService.getAllAdminRoles();
+    @GetMapping(value = "")
+    public BaseApiResponse getRoles(WebRequest request){
+        return adminRoleService.getRoles(request.getParameterMap());
     }
 
-    @GetMapping("/{id}")
-    public Object getAllAdminRoleById(@PathVariable("id") Long id){
-        return adminRoleService.getAllAdminRoleById(id);
+    @PostMapping(value = "/")
+    public BaseApiResponse addRole(@RequestBody AdminRole role, HttpServletRequest request, Principal principal){
+        return adminRoleService.addRole(role, request, principal);
     }
 
-    @PostMapping
-    public void addAdminRole(@RequestBody AdminRole body){
-        adminRoleService.addAdminRole(body);
+    @PutMapping(value = "/{id}")
+    public BaseApiResponse updateRole(@RequestBody String roleName,
+                                      HttpServletRequest request,
+                                      @PathVariable long id,
+                                      Principal principal){
+        return adminRoleService.updateRole(roleName, id, request, principal);
+    }
+
+    @GetMapping(value = "/permissions/in/{roleId}")
+    public BaseApiResponse getPermissionsInRole(@PathVariable long roleId, WebRequest request){
+        return adminRoleService.getAllocatedPemissions(roleId,request.getParameterMap());
+    }
+
+    @GetMapping(value = "/permissions/notin/{roleId}")
+    public BaseApiResponse getPermissionsNotInRole(@PathVariable long roleId, WebRequest request){
+        return adminRoleService.getUnAllocatedPemissions(roleId,request.getParameterMap());
+    }
+
+    @PostMapping(value = "/allocate/{roleId}")
+    public BaseApiResponse allocatePermissionsToRole(@PathVariable long roleId,
+                                                     HttpServletRequest request,
+                                                     @RequestBody String body,
+                                                     Principal principal){
+
+        return adminRoleService.allocatePermissions(body, roleId, request, principal);
+    }
+
+    @PostMapping(value = "/deallocate/{roleId}")
+    public BaseApiResponse deAllocatePermissionsToRole(@PathVariable long roleId,
+                                         HttpServletRequest request,
+                                         @RequestBody String body,
+                                         Principal principal){
+        return adminRoleService.deallocatePermissions(body,roleId, request, principal);
     }
 
 
--- src/main/groovy/com/poosh/event/management/adminrole/AdminRoleService.groovy
+++ src/main/groovy/com/poosh/event/management/adminrole/AdminRoleService.groovy
@@ -1,23 +1,34 @@
 package com.poosh.event.management.adminrole
 
 import com.poosh.event.management.apiresponse.BaseApiResponse
+import com.poosh.event.management.audit.AuditService
+import com.poosh.event.management.exceptions.InternalServerErrorException
+import com.poosh.event.management.utils.CommonDbFunctions
 import com.poosh.event.management.utils.MyUtil
 import groovy.json.JsonSlurper
 import groovy.sql.Sql
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 
+import javax.servlet.http.HttpServletRequest
 import javax.sql.DataSource
+import java.security.Principal
 
 @Service
 class AdminRoleService {
 
     private final AdminRoleRepository adminRoleRepository
     private final DataSource dataSource
+    private final AuditService auditService
 
     @Autowired //inject the object dependency implicitly
-    public AdminRoleService(AdminRoleRepository adminRoleRepository) {
+    AdminRoleService(AdminRoleRepository adminRoleRepository,
+                     DataSource dataSource,
+                     AuditService auditService) {
         this.adminRoleRepository = adminRoleRepository
+        this.dataSource = dataSource
+        this.auditService = auditService
     }
 
     BaseApiResponse getRoles(Map parameterMap) {
@@ -43,11 +54,18 @@ class AdminRoleService {
 
     }
 
-    public Long addRole(AdminRole role) {
+    BaseApiResponse addRole(AdminRole role, HttpServletRequest request, Principal principal) {
         Sql sql = new Sql(dataSource);
+        BaseApiResponse res = new BaseApiResponse(HttpStatus.OK.value(), "role added successfully")
+        def currentUser = principal.getName()
         def insertRes = sql.executeInsert("INSERT INTO admin_roles(name) VALUES (?.name)", role);
         sql.close();
-        return insertRes?.get(0)?.get(0);
+        def newRoleId = insertRes?.get(0)?.get(0)
+        if(newRoleId==null){
+            throw new InternalServerErrorException("failed to add role")
+        }
+        auditService.logAuditEvent("Add user role",request.getRemoteAddr(),currentUser,insertRes+"");
+        return res
     }
 
     BaseApiResponse getAllocatedPemissions(long roleId, Map parameterMap) {
@@ -77,7 +95,6 @@ class AdminRoleService {
     }
 
     BaseApiResponse getUnAllocatedPemissions(long roleId, Map parameterMap) {
-
         def params = MyUtil.flattenListParam(parameterMap);
         def start = params.start?.toInteger();
         def limit = params.limit?.toInteger();
@@ -102,11 +119,15 @@ class AdminRoleService {
 
     }
 
-    BaseApiResponse allocatePermissions(String permissionsJsonStr, long userId, long roleId) {
-
+    BaseApiResponse allocatePermissions(String permissionsJsonStr,
+                                        long roleId,
+                                        HttpServletRequest request,
+                                        Principal principal) {
+        String currentUser = principal.getName()
+        long userId = CommonDbFunctions.getUserIdFromEmail(currentUser)
         def sqlParams = [userId: userId, roleId: roleId];
         Sql sql = new Sql(dataSource);
-        def res = [success: true, status: 0];
+        BaseApiResponse res = new BaseApiResponse(HttpStatus.OK.value(), "allocation successful")
         List permissions = new JsonSlurper().parseText(permissionsJsonStr);
         sql.withTransaction {
             permissions.each {
@@ -114,16 +135,19 @@ class AdminRoleService {
                 sqlParams.permissionId = permissionId;
                 sql.executeInsert("INSERT INTO admin_role_permissions(role_id, permission_id, allocated_by) VALUES (?.roleId, ?.permissionId, ?.userId)", sqlParams);
             }
-            res.put('status', 1);
         }
         sql.close();
+        auditService.logAuditEvent("Allocate user role",request.getRemoteAddr(),currentUser,"User:"+userId+",Role:"+roleId)
         return res
 
     }
 
-    BaseApiResponse deallocatePermissions(String permissionsJsonStr, long roleId) {
+    BaseApiResponse deallocatePermissions(String permissionsJsonStr,
+                                          long roleId,
+                                          HttpServletRequest request,
+                                          Principal principal) {
         Sql sql = new Sql(dataSource);
-        def res = [success: true, status: 0];
+        BaseApiResponse res = new BaseApiResponse(HttpStatus.OK.value(), "deallocation successful")
         def sqlParams = [roleId: roleId];
 
         List permissions = new JsonSlurper().parseText(permissionsJsonStr);
@@ -133,22 +157,27 @@ class AdminRoleService {
                 sqlParams.permissionId = permissionId;
                 sql.executeInsert("DELETE  FROM admin_role_permissions WHERE role_id = ?.roleId AND permission_id = ?.permissionId", sqlParams);
             }
-            res.put('status', 1);
         }
-        sql.close();
+        sql.close()
+        String currentUser = principal.getName()
+        long userId = CommonDbFunctions.getUserIdFromEmail(currentUser)
+        auditService.logAuditEvent("Deallocate user role",request.getRemoteAddr(),currentUser,"User:"+userId+",Role:"+roleId);
         return res
-
     }
 
 
-    public long updateRole(String roleName, Long roleId) {
-        DriverManagerDataSource dataSource = DataSourceFactory.getApplicationDataSource();
-        Sql sql = new Sql(dataSource);
-        Map res = [success: true, status: 0, message: ""];
-        Map params = new JsonSlurper().parseText(roleName);
-        params.put("roleId", roleId);
-        int updateRes = sql.executeUpdate("UPDATE admin_roles SET name = ?.name WHERE id = ?.roleId", params);
+    BaseApiResponse updateRole(String roleName, Long roleId, HttpServletRequest request, Principal principal) {
+        Sql sql = new Sql(dataSource)
+        BaseApiResponse res = new BaseApiResponse(HttpStatus.OK.value(), "update successful")
+        Map params = new JsonSlurper().parseText(roleName)
+        params.put("roleId", roleId)
+        int updateRes = sql.executeUpdate("UPDATE admin_roles SET name = ?.name WHERE id = ?.roleId", params)
+        if(updateRes < 1){
+            throw new InternalServerErrorException("update failed")
+        }
+        String currentUser = principal.getName()
+        auditService.logAuditEvent("Update user role",request.getRemoteAddr(),currentUser," Role:"+roleId)
         sql.close();
-        return updateRes;
+        return res;
     }
 }
